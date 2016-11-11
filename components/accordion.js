3cdbbf9f4d1c29ff1bfe90f8521efa51e1d79f44
Gavin King <gavin.king@itslearning.com>
2016-11-11 15:07:21 +0100

    Data driven component

--- components/accordion.js
+++ components/accordion.js
@@ -1,12 +1,33 @@
 // Let's go completely vanilla
-function Accordion(element) {
-    // How will we construct this in the simplest way possible?
-    this.element = element;
+function Accordion(viewData) {
+    this.element = null;
 
-    this.element.querySelector('h1').addEventListener('click', () => this.toggle());
+    // How will we construct this in the simplest way possible?
+    this._render(viewData);
+    this._bindUI();
 }
 
 Accordion.prototype.toggle = function () {
     // What should happen when we want to 'close' the accordion?
     this.element.classList.toggle('closed');
 };
+
+Accordion.prototype._bindUI = function () {
+    this.element.querySelector('h1').addEventListener('click', () => this.toggle());
+};
+
+Accordion.prototype._render = function (viewData) {
+    const markup = `
+        <article id="${viewData.id}" class="accordion">
+            <h1><button>${viewData.title}</button></h1>
+            <div class="content">
+                ${viewData.content}
+            </div>
+        </article>`,
+
+        container = document.createElement('div');
+
+    container.innerHTML = markup;
+
+    this.element = container.children[0];
+}
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', init);
 
+let appStore = {
+    accordion: {
+        'id': 'my-accordion',
+        'title': 'My title',
+        'content':
+            `<p>
+                This is some content and it probably came from a service
+                of some kind. I doubt you have hardcoded content in your
+                application.
+            </p>`
+    }
+};
+
 function init() {
     // Setup one Accordion
-    const
-        accordionElement = document.getElementById('my-accordion'),
-        accordion1 = new Accordion(accordionElement);
+    const accordion1 = new Accordion(appStore.accordion);
+
+    document.body.appendChild(accordion1.element);
 }
