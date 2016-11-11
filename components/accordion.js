62466f3d4e4a194c901855bdcd564f28a678d7f5
Gavin King <gavin.king@itslearning.com>
2016-11-11 14:45:19 +0100

    A working accordion

--- components/accordion.js
+++ components/accordion.js
@@ -1,8 +1,12 @@
 // Let's go completely vanilla
-function Accordion() {
+function Accordion(element) {
     // How will we construct this in the simplest way possible?
+    this.element = element;
+
+    this.element.querySelector('h1').addEventListener('click', () => this.toggle());
 }
 
 Accordion.prototype.toggle = function () {
     // What should happen when we want to 'close' the accordion?
+    this.element.classList.toggle('closed');
 };
--- main.js
+++ main.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     // Setup one Accordion
-    const accordion1 = new Accordion();
+    const
+        accordionElement = document.getElementById('my-accordion'),
+        accordion1 = new Accordion(accordionElement);
 }
